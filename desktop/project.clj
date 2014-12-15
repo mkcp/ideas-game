19c4cf80a5ff8580cd5b37202c807b34570d68d4
Mike Patella <m.c.patella@gmail.com>
2014-12-15 03:18:42 -0500

    update the description

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,6 +1,6 @@
 (defproject ideas-game "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  
+  :description "If you touch an idea it will explode."
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.4.1"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.4.1"]
                  [com.badlogicgames.gdx/gdx-box2d "1.4.1"]
@@ -13,7 +13,7 @@
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
                  [play-clj "0.4.2"]]
-  
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [ideas-game.core.desktop-launcher]
